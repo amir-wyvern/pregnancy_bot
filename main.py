cdbc26101d421d734d0248b29219fdcb8fb147b4
wyvern <amirhosein_wyvern@yahoo.com>
2024-10-12 01:00:53 +0330

    fix: add initial database

--- main.py
+++ main.py
@@ -7,30 +7,36 @@ from telegram.ext import (
     CallbackQueryHandler
 )
 
+from configs.settings import settings
+
 from handlers.start_handler import start
-from handlers.command_handler import logout
-from dotenv import load_dotenv
-import os
-from pathlib import Path
+# from handlers.command_handler import logout
 
 from handlers.callback_handler import callback_handler
 from handlers.message_handler import message_handler
 
-dotenv_path = Path('.env')
-load_dotenv(dotenv_path=dotenv_path)
+from db.initial import init_database
+from schemas import AdminRegisterForDataBase
 
-
-TOKEN_BOT = os.getenv('BOT_TOKEN')
 
 root_logger = logging.getLogger()
 root_logger.handlers.clear()
 
+admin_default = AdminRegisterForDataBase(
+    username= settings.ADMIN_USERNAME_DEFAULT,
+    password= settings.ADMIN_PASSWORD_DEFAULT,
+    tel_id= settings.ADMIN_TEL_ID_DEFAULT,
+    status= True
+)
+
+init_database(admin_default, root_logger)
+
 if __name__ == '__main__':
 
-    application = ApplicationBuilder().token(TOKEN_BOT).build()
+    application = ApplicationBuilder().token(settings.BOT_TOKEN).build()
     
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('logout', logout))
+    # application.add_handler(CommandHandler('logout', logout))
     application.add_handler(MessageHandler(filters.TEXT, message_handler))
     # application.add_handler(MessageHandler(filters.CONTACT, phonenumber_handler))
     application.add_handler(CallbackQueryHandler(callback_handler))
