2e5fc6519fa6119dda8ceb70da190235e07fb49c
wyvern <amirhosein_wyvern@yahoo.com>
2024-10-12 00:57:38 +0330

    feat: add more envs

--- configs/settings.py
+++ configs/settings.py
@@ -6,6 +6,10 @@ class Settings(BaseSettings):
     CACHE_URL: str
     SQLITE_ADDRESS: str
 
+    ADMIN_USERNAME_DEFAULT: str
+    ADMIN_PASSWORD_DEFAULT: str
+    ADMIN_TEL_ID_DEFAULT: int
+    
     class Config:
         env_file = '.env'
 
